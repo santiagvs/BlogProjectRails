18db35c69929b16df6f6d106c09b5a56e1a2e7a4
Santiago <victor.lacsilva0102@gmail.com>
2022-07-07 21:01:30 -0300

    feat/refactor: created new partials, DRYed code and refactored code and stylization

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@popperjs/core")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { Tooltip } from "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
+
+})
 
 Rails.start()
 Turbolinks.start()
